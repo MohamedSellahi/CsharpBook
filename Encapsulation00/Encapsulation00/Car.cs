6eec226fae7f5efd23ab8270f1c43c0d27acc953
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-03-30 16:43:46 +0200

    explicit Interfaces

---
+++ Encapsulation00/Encapsulation00/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation00 {
+   class Car {
+      // the state of the car 
+      public string petName;
+      public int currSpeed;
+
+      
+      // A custom default constructor 
+      public Car() {
+         petName = "Chuck";
+         currSpeed = 10;
+      }
+
+      public Car(string name, int speed) {
+         petName = name;
+         currSpeed = speed;
+      }
+
+      // the functionality of the car
+      public void PrintState() {
+         Console.WriteLine("{0} is going {1} KMh.", petName, currSpeed);
+      }
+
+      public void SpeedUp(int delta) {
+         currSpeed += delta;
+      }
+
+   }// end of class
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism {
+  class Program {
+    static void Main(string[] args) {
+
+      MiniVan van = new MiniVan(50, "Smart");
+      van.DisplayStats();
+
+      // nested class usage 
+      NestedOuter.PublicInnerClass inner = new NestedOuter.PublicInnerClass(5);
+      NestedOuter.PublicInnerClass.innerEnum enm = NestedOuter.PublicInnerClass.innerEnum.Item1;
+      Console.WriteLine(enm);
+
+
+      // 
+      Console.WriteLine("#####");
+      Console.WriteLine("#");
+      Console.WriteLine("###");
+      Console.WriteLine("#");
+      Console.WriteLine("#");
+      //
+      Console.WriteLine("----------------");
+      ThreeDCercle cer = new ThreeDCercle();
+      cer.Draw(); // calls 3d implementation 
+      ((Cercle)cer).Draw(); // cast force calling draw of cercle class
+      
+
+
+    }// main 
+
+
+
+
+  }
+}
