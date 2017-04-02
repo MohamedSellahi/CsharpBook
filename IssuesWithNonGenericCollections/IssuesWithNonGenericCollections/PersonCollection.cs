ba14d89c465ee105ca1f8a2fc5ab44cdfb1b08c6
Mohamed <Mohamed@MOHAMED-PC>
2017-04-02 21:24:35 +0200

    BoxingUnboxingDilemma

---
+++ IssuesWithNonGenericCollections/IssuesWithNonGenericCollections/PersonCollection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// strongly typed custom collection of people 
+namespace IssuesWithNonGenericCollections {
+  class PersonCollection : IEnumerable {
+
+    private ArrayList arPeople = new ArrayList();
+
+    public PersonCollection() {}
+    // Cast for the caller 
+    public Person GetPerson(int pos) {
+      return (Person)arPeople[pos];
+    }
+
+    // Insert only person objects 
+    public void AddPerson(Person p) {
+      arPeople.Add(p);
+    }
+
+    public void ClearPeople() {
+      arPeople.Clear();
+    }
+
+    public int Count { get { return arPeople.Count; } }
+
+    // Foreach enumeration support
+    // make it explicit implementation. can't be called from 
+    // object level
+
+    IEnumerator IEnumerable.GetEnumerator() {
+      return arPeople.GetEnumerator();
+    }
+
+  }
+}
---
+++ IssuesWithNonGenericCollections/IssuesWithNonGenericCollections/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssuesWithNonGenericCollections {
+  class Program {
+    static void Main(string[] args) {
+      // Value types are autmatically boxed when 
+      // passed to a member requiring an object
+
+      ArrayList myInts = new ArrayList();
+      for (int j = 0; j < 10; j++) {
+        myInts.Add(j);
+      }
+
+      // unbooxing occurs when an object to converted back to 
+      // stack-based datatypes 
+      int i = (int)myInts[0];
+
+      // Now it is reboxed as WriteLine() requires object types!
+      Console.WriteLine("Value of your int is {0}", i);
+
+      Console.WriteLine("\n***** Custom Person Collection ******\n");
+      PersonCollection myPeople = new PersonCollection();
+      for (int j = 0; j < 5; j++) {
+        myPeople.AddPerson(new Person("fname" + j, "lname" + j, 2 * j + 30));
+      }
+
+      foreach (Person item in myPeople) {
+        Console.WriteLine(item);
+      }
+    }
+
+    static void SimpleBoxUnboxOperation() {
+      // Make a value type (int) variable 
+      int myInt = 25;
+      // box the int into an object reference 
+      object boxedInt = myInt;
+
+      // Unbox the reference back to a corresponding int;
+      int unBoxedInt = (int)boxedInt;
+    }
+
+
+
+  }
+}
