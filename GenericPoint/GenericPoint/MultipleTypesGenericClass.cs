df0caf2a0bad23417c19d4b86f0ab7ba298a021e
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-04-03 16:05:16 +0200

    End collectionand Generics

---
+++ GenericPoint/GenericPoint/MultipleTypesGenericClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPoint {
+  class MultipleTypesGenericClass<T,K> 
+    where T: new() 
+    where K: new(){
+
+  }
+}
---
+++ GenericPoint/GenericPoint/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPoint {
+  class Program {
+    static void Main(string[] args) {
+
+      // point using int
+      Point<int> pint = new Point<int>(10, 10);
+
+      // point using double
+      Point<double> pd = new Point<double>(5.3, 3.3);
+      Console.WriteLine(pint);
+      Console.WriteLine(pd);
+      pint.ResetPoint();
+      pd.ResetPoint();
+      Console.WriteLine(pint);
+      Console.WriteLine(pd);
+      
+    }
+
+   
+  }
+}
