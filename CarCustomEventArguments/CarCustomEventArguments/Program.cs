17d88b85718793cdd7001a969b64d935b7c96cef
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-04-04 16:41:28 +0200

    SecondCmmitCustomEventArgs

--- CarCustomEventArguments/CarCustomEventArguments/Program.cs
+++ CarCustomEventArguments/CarCustomEventArguments/Program.cs
@@ -10,14 +10,19 @@ namespace CarCustomEventArguments {
       Car c1 = new Car("FORD", 200, 10);
 
       // Subscribing to the event 
-
+      c1.AboutToBlow += Car_AboutToBlow;
 
 
     }
 
+    private static void Car_AboutToBlow(object sender, CarEventArgs e) {
+      if(sender is Car) {
+        Car c = (Car)sender;
+        Console.WriteLine("critical message from {0}: {1}", c.PetName, e._message);
+
+      }
 
 
-
-
+    }
   }
 }
