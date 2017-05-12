4d64c7162a6c6984cac25b2297481a775417514c
Mohamed <mohamed.sellahi@gmail.com>
2017-05-12 10:45:26 +0200

    End Linq Queries Part1

--- LinqExpressions/LinqExpressions/Program.cs
+++ LinqExpressions/LinqExpressions/Program.cs
@@ -22,8 +22,33 @@ namespace LinqExpressions {
       SelectEverything(itemsInStock);
       Console.WriteLine('\n');
       ListProductnaes(itemsInStock);
+
       Console.WriteLine('\n');
       GetOverStock(itemsInStock);
+
+      Console.WriteLine('\n');
+      GetGamesAndDescription(itemsInStock);
+
+      Console.WriteLine('\n');
+      Console.WriteLine("Using projected data ");
+      /* using the Array type: can hold any object */
+      Array objs = GetProjectedData(itemsInStock);
+      foreach (object item in objs) {
+        Console.WriteLine(item);
+      }
+
+      Console.WriteLine('\n');
+      GetcountFromQuery();
+
+      Console.WriteLine('\n');
+      ReverseEveryThing(itemsInStock);
+
+      Console.WriteLine('\n');
+      AlphabetizedProductNames(itemsInStock);
+
+
+
+
 
 
 
@@ -63,8 +88,61 @@ namespace LinqExpressions {
       
     }
 
+    /* Projecting New Data Types */
+    static void GetGamesAndDescription(ProductInfo[] products){
+      Console.WriteLine("Names and Description");
+      var nameDesc = from p in products
+                     select new { p.Name, p.Description }; // store in anonymous type
+      foreach (var item in nameDesc) {
+        Console.WriteLine(item.ToString());
+      }
 
 
+    }
+
+    /* returning projected subsets */
+    static Array GetProjectedData(ProductInfo[] products) {
+      
+      var nameDesc = from p in products
+                     select new {
+                       p.Name, p.Description
+                     };
+      // Map the anponymous type to an Array of objects 
+      return nameDesc.ToArray();
+    }
+
+    /* Obtaining count using Enumerable */
+    static void GetcountFromQuery() {
+      string[] games = {"Morrowind", "Uncharted 2","Fallout 3", "Daxter", "System Shock 2"};
+
+      // Get count from query 
+      int numb = (from g in games
+                  where g.Length > 6
+                  select g).Count();
+
+      // print out the number 
+      Console.WriteLine("{0} items honor the LinQ Query", numb);
+    }
+
+    /* Reversing result sets */
+    static void ReverseEveryThing(ProductInfo[] product) {
+      Console.WriteLine("Product in reverse.");
+      var allProducts = (from p in product select p).Reverse();
+      foreach (var item in allProducts) {
+        Console.WriteLine(item);
+      }
+    }
+
+    /*sorting expressions */
+    static void AlphabetizedProductNames(ProductInfo[] product) {
+      // Get Names of products in alphabetic order 
+      var subset = from p in product orderby p.Name select p;
+      Console.WriteLine("Ordered by Name ");
+      foreach (var item in subset) {
+        Console.WriteLine(item);
+      }
+
+    }
 
 
   }
