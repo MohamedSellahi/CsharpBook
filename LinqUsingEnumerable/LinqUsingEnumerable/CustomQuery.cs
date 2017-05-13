b95d9c28ae027ccc537a3aa12268034899128c58
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-05-13 08:07:37 +0200

    LinqUsingEnumerable

---
+++ LinqUsingEnumerable/LinqUsingEnumerable/CustomQuery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqUsingEnumerable {
+  class CustomQuery {
+
+    
+    /* building queries using raw delegates */
+    public static void QuaryStringWithRawDelegate() {
+      Console.WriteLine("***** Using Raw Delegates *****");
+      string[] Games = {"Morrowind", "Uncharted 2",
+        "Fallout 3", "Daxter", "System Shock 2"};
+      /* build the necessary delegates */
+      Func<string, bool> searchFilter = new Func<string, bool>(Filter);
+      Func<string, string> itemPrecessor = new Func<string, string>(ProcessItem);
+      // pass the delegates to the methods of Enumerable 
+      var subset = Games.Where(searchFilter)
+                        .OrderBy(itemPrecessor)
+                        .Select(itemPrecessor);
+
+      foreach(var item in subset) {
+        Console.WriteLine("Item: {0}", item);
+      }
+
+    }
+
+
+
+    /*delegate targets */
+    public static bool Filter(string s) { return s.Contains(" "); }
+    public static string ProcessItem(string s) { return s; }
+  }
+}
