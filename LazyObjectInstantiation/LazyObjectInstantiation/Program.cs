54f031f73fae90fd0d272dccfa761066d90e5d62
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-05-15 09:36:38 +0200

    Lazy object Intantiation

---
+++ LazyObjectInstantiation/LazyObjectInstantiation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyObjectInstantiation {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("***** Fun with Lazy instantiation *****");
+      /* this callser does not care obout getting all songs 
+       * but indirectly created 10000 objects!
+       * */
+       // here the allocation of allsongs will not happen since
+       // the member has not been used from code 
+      MediaPlayer myPlayer = new MediaPlayer();
+      myPlayer.Play();
+
+      /* here we the CLR will instantiate allSongs since 
+       * it is used in the code */
+      MediaPlayer myPlayer2 = new MediaPlayer();
+      myPlayer2.GetAllTracks();
+
+      /* Lazy object instantiation is usefull to decrease unnecessary
+       * allocation of objects. it is also used for members who have
+       * expensive creation code, such as invoking remote methods 
+       * communication with relationnal databases and soforth . */
+       
+    }
+
+
+  }
+}
---
+++ LazyObjectInstantiation/LazyObjectInstantiation/Song.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LazyObjectInstantiation {
+  class Song {
+    public string Artist { get; set; }
+    public string TrackName { get; set; }
+    public double TrackLength { get; set; }
+  }
+
+  class AllTracks {
+    // Our media player can have a maximum 
+    // of 10,000 songs 
+    private Song[] allSongs = new Song[10000];
+
+    public AllTracks() {
+      // assume we fill up the array 
+      // of songs 
+      Console.WriteLine("Filling up the songs!");
+    }
+
+    public AllTracks(string str) {
+      Console.WriteLine("Filling up the songs! using " + str);
+    }
+  }
+
+  // The MediaPlayer has-an AllTracks object.
+  class MediaPlayer {
+    public void Play() { Console.WriteLine("playing Songs..."); }
+    public void Pause() { }
+    public void Stop() { }
+
+    //private AllTracks allSongs = new AllTracks();
+    /* replace this declaration with */
+
+    //private Lazy<AllTracks> allSongs = new Lazy<AllTracks>();
+
+    // this line of code will not instantiate allSongs
+    // unless it is used somewhere in the code 
+
+    /* Use lambda expression additionnal code when 
+     * the tracks are created. a custom constructor can be specified
+     */
+    private Lazy<AllTracks> allSongs = new Lazy<AllTracks>(() => {
+      Console.WriteLine("Creating All tracks object!");
+      //return new AllTracks();
+      return new AllTracks("a custom constructor");
+    }
+    );
+
+
+    public AllTracks GetAllTracks() {
+      // return all the songs
+      // return allSongs;//<-- replace this line 
+      return allSongs.Value;
+    }
+
+  }
+
+}
