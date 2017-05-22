85f1bbcd0823306a7f8412bc2ae84c1db56fc439
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-05-22 16:45:57 +0200

    .NET Attributes

---
+++ AttributedCarLibrary/AttributedCarLibrary/VehicleDescriptionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributedCarLibrary {
+  /* Restricting the use of attributes to some kind of codes 
+  * we can use [Attributeusage] attribute and supply any 
+  * combination of values from Attributetargets enum 
+  * see new definition of 
+  **/
+  public sealed class VehicleDescriptionAttribute: Attribute {
+    public string Description { get; set; }
+
+    public VehicleDescriptionAttribute() {}
+    public VehicleDescriptionAttribute(string vehicleDescription) {
+      Description = vehicleDescription;
+    }
+
+  }
+ 
+
+}
