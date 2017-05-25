c9d6fafa04955f908c4ac0c509c95a25af0f47a2
Mohamed <mohamed.sellahi@gmail.com>
2017-05-25 19:21:26 +0200

    DynamicTyping

---
+++ DynamicKeyword/DynamicKeyword/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicKeyword {
+  class Program {
+    static void Main(string[] args) {
+    }
+
+    static public void ImplicitlyTypedVariable() {
+      var a = new List<int>();
+      a.Add(90);
+
+      // this would be a compile time error 
+      //a = "hello";
+    }
+
+
+  }
+
+}
