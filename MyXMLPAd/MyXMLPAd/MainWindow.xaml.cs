ee94d5480b8f9374b104f715c98a186ff6082782
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-06-01 16:42:57 +0200

    init data binding

---
+++ MyXMLPAd/MyXMLPAd/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Windows.Markup;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyXMLPAd {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+    }
+    private string _filePath = "MyXAML.xaml";
+
+
+
+    // 
+    private void Window_Loaded(object sender, RoutedEventArgs e) {
+      // Check if there is an XAML file in the working directory 
+      // if so laod it, otherwise fill in the textBox area with
+      // default template 
+      
+      if (File.Exists(_filePath)) {
+        txtXamlData.Text = File.ReadAllText(_filePath);
+      }
+      else {
+        txtXamlData.Text =
+        "<Window xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"\n"
+        + "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\"\n"
+        + "Height =\"400\" Width =\"500\" WindowStartupLocation=\"CenterScreen\">\n"
+        + "<StackPanel>\n"
+        + "</StackPanel>\n"
+        + "</Window>";
+      }
+
+    }
+
+
+
+    private void Window_Closed(object sender, EventArgs e) {
+      // Write out the data in the text block to a local *.xmal file. 
+      File.WriteAllText(_filePath, txtXamlData.Text);
+      Application.Current.Shutdown();
+    }
+
+   
+
+    private void btnViewXaml_Click(object sender, RoutedEventArgs e) {
+      // Write out the data in the text block to a local *.xaml file 
+      File.WriteAllText(_filePath, txtXamlData.Text);
+
+      // this is the window that will be constructed 
+      Window win = new Window();
+      try {
+        using (Stream sr = File.Open(_filePath,FileMode.Open)) {
+          // Connect the XAML to the window 
+          win = (Window)XamlReader.Load(sr);
+          win.ShowDialog();
+          win.Close();
+          win = null; 
+        }
+
+      }
+      catch (Exception ex) {
+        Console.WriteLine(ex.Message);
+      }
+
+    }
+
+
+
+  }
+}
---
+++ WpfControlAndAPI/WpfControlAndAPI/MainWindow.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.IO;
+using System.Windows.Ink;
+using System.Windows.Markup;
+using Microsoft.Win32;
+using System.Windows.Annotations;
+using System.Windows.Annotations.Storage;
+
+namespace WpfControlAndAPI {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+
+      // set default values 
+      this.myCanvas.EditingMode = InkCanvasEditingMode.Ink;
+      this.inkRadio.IsChecked = true;
+      this.comboColors.SelectedIndex = 0;
+
+      // Document tab 
+      Populatedocument();
+
+      // 
+      EnableAnnotations();
+     
+    }
+
+    private void EnableAnnotations() {
+      // Create AnnotationService object that will 
+      // work with myReader 
+      AnnotationService annoService = new AnnotationService(this.myReader);
+
+      // Create a memorystream that will hold the annotations 
+      MemoryStream annoStream = new MemoryStream();
+
+      // Now, create an XML-based store on the memory stream
+      // You could use this object to programatically add, delete
+      // or find annotations
+      AnnotationStore store = new XmlStreamStore(annoStream);
+     
+      // Enbable the annotation services 
+      annoService.Enable(store);
+     
+
+    }
+
+    private void inkRadio_Click(object sender, RoutedEventArgs e) {
+      // based on which button sent the event, place the Inkcanvas in a 
+      // unik mode 
+      switch ((sender as RadioButton).Content.ToString()) {
+        case "Ink Mode!":
+          myCanvas.EditingMode = InkCanvasEditingMode.Ink;
+          break;
+        case "Erase Mode!":
+          myCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+          break;
+        case "Select Mode!":
+          myCanvas.EditingMode = InkCanvasEditingMode.Select;
+          break;
+
+      }
+
+    }
+
+    private void ColorChanged(object sender, SelectionChangedEventArgs e) {
+      // Get the selected value in the combo box 
+      // string colorTouse = (this.comboColors.SelectedItem as ComboBoxItem).Content.ToString();
+
+      // Get the tag of the selected panel 
+      string colorToUse = (this.comboColors.SelectedItem as StackPanel).Tag.ToString();
+
+      
+      // Change the color used to render the strokes.
+      this.myCanvas.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString(colorToUse); 
+    }
+
+    private void btnSave_Click(object sender, RoutedEventArgs e) {
+      // open dialog box 
+      SaveFileDialog svdlg = new SaveFileDialog();
+      string fileName = null;
+      svdlg.Filter = "CanvasData | *.bin";
+      if ((bool)svdlg.ShowDialog(this) == true) {
+        // try to save the file
+        fileName = svdlg.FileName;
+        //Save all Data on the canvas to a local file 
+        try {
+          using (FileStream fs = new FileStream(fileName, FileMode.Create)) {
+            this.myCanvas.Strokes.Save(fs);
+            fs.Close(); // is it realy necessary 
+          }
+
+        }
+        catch (Exception ex) {
+          MessageBox.Show(ex.Message, "Error saving file");
+        }
+
+      }
+
+
+      
+    }
+
+    private void btnLoad_Click(object sender, RoutedEventArgs e) {
+
+      OpenFileDialog dlg = new OpenFileDialog();
+      dlg.Filter = "canvas Data | *.bin";
+      string fileName = null; 
+      if ((bool)(dlg.ShowDialog()) == true) {
+        // try to open 
+        fileName = dlg.FileName;
+        try {
+
+          using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read)) {
+            StrokeCollection strockes = new StrokeCollection(fs);
+            myCanvas.Strokes = strockes;
+          }
+        }
+        catch (Exception ex ) {
+          MessageBox.Show(ex.Message, "Error opening file");
+        }
+
+      }
+      // Fill StrockCollection from file 
+
+
+      
+    }
+
+    private void btnClear_Click(object sender, RoutedEventArgs e) {
+      // Clear the canvas 
+      this.myCanvas.Strokes.Clear();
+      
+    }
+
+    // 
+    private void Populatedocument() {
+      // Add some data to the list item
+      this.listOfFuncFacts.FontSize = 14;
+      this.listOfFuncFacts.MarkerStyle = TextMarkerStyle.Circle;
+      this.listOfFuncFacts.ListItems.Add(new ListItem(
+        new Paragraph(new Run("Fixed Document are for WYSIWYG print ready docs"))));
+      this.listOfFuncFacts.ListItems.Add(new ListItem(new Paragraph(new Run("The Api supports tables and embeded figures"))));
+      this.listOfFuncFacts.ListItems.Add(new ListItem(new Paragraph(new Run("flow Documents are read only"))));
+      this.listOfFuncFacts.ListItems.Add(new ListItem(new Paragraph(new Run("BlockUIContainer allows you to embed WPF controls in the document"))));
+
+      // Now add some data to the paragraph
+      // First part of sentence
+      Run prefix = new Run("this paragraph was generated");
+
+      // Middle of paragraph
+      Bold b = new Bold();
+      Run infix = new Run("dynamicaly");
+      infix.Foreground = Brushes.Red;
+      infix.FontSize = 30;
+      b.Inlines.Add(infix);
+      // last part of the paragraph
+      Run suffix = new Run("at runtime!");
+
+      // now each pieace to the inline collection elements 
+      // of the paragraph 
+      this.parbodyText.Inlines.Add(prefix);
+      this.parbodyText.Inlines.Add(b);
+      this.parbodyText.Inlines.Add(suffix);
+
+
+      // laod save document event handlers 
+      btnSaveDoc.Click += (o, args) => {
+        SaveFileDialog svDlg = new SaveFileDialog();
+        svDlg.Filter = "XML data | *.XML";
+
+        if ((bool)(svDlg.ShowDialog() == true)) {
+          string fileName = svDlg.FileName; 
+
+          using (FileStream fs = File.Open(fileName,FileMode.Create)) {
+            XamlWriter.Save(this.myReader.Document,fs);
+          }
+        }
+      };
+
+      btnLoadDoc.Click += (o, s) => {
+        OpenFileDialog opdlg = new OpenFileDialog();
+        opdlg.Filter = "XML Data | *.XML";
+        if ((bool)(opdlg.ShowDialog()) == true) {
+          string fileName = opdlg.FileName;
+          using (FileStream fs = File.Open(fileName, FileMode.Open)) {
+            try {
+              FlowDocument doc = XamlReader.Load(fs) as FlowDocument;
+              this.myReader.Document = doc;
+            }
+            catch (Exception ex) {
+              MessageBox.Show(ex.Message, "Error Loading Doc!");
+            }
+          }
+         
+
+        }
+      };
+
+
+      
+      
+      
+
+    }
+
+  }
+}
