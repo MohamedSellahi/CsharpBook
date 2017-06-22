c6a23390cff50d5b49e835b560183616ae2673b2
Mohamed <mohamed.sellahi@gmail.com>
2017-06-22 23:18:19 +0200

    Timer Type

---
+++ TimerApp/TimerApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TimerApp {
+   class Program {
+      static void Main(string[] args) {
+         Console.WriteLine("***** Working with Timer Type *****");
+         // Create a Tymer callBack 
+         TimerCallback timeCB = new TimerCallback(PrintTime);
+
+         // Establish Timer setting 
+         Timer t = new Timer(timeCB, null, 0, 1000);
+         //
+         Console.WriteLine("Hit any Key to terminate ");
+         Console.ReadLine();
+      }
+
+      public static void PrintTime(object state) {
+         Console.WriteLine("Time is: {0}", DateTime.Now.ToLongTimeString());
+      }
+   }
+}
