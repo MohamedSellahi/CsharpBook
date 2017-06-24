5bb73baf3e56f781f6d46442c907dcb1b88cc121
Mohamed <mohamed.sellahi@gmail.com>
2017-06-24 13:24:07 +0200

    DataProviderFactory

--- DataProviderFactory/DataProviderFactory/Program.cs
+++ DataProviderFactory/DataProviderFactory/Program.cs
@@ -1,49 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
 using System.Configuration;
 using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace DataProviderFactory {
-  class Program {
-    static void Main(string[] args) {
-      Console.WriteLine("***** Fun With Data Providers *****");
-      // Get connection string/provider from *.config
-      string dp = ConfigurationManager.AppSettings["provider"];
-      string cnStr = ConfigurationManager.AppSettings["cnStr"];
-      //string dp = ConfigurationManager.AppSettings["oledbProvider"];
-      //string cnStr = ConfigurationManager.AppSettings["cnStrOledb"];
+   class Program {
+      static void Main(string[] args) {
+         Console.WriteLine("***** Fun With Data Providers *****");
 
-      // get factory provider 
-      DbProviderFactory df = DbProviderFactories.GetFactory(dp);
+         // Get connection string/provider from *.config
+         string dp = ConfigurationManager.AppSettings["provider"];
+         //string cnStr = ConfigurationManager.AppSettings["cnStr"];
+         string cnStr = ConfigurationManager.ConnectionStrings["AutoLotSqlProvider"].ConnectionString;
 
-      // now get the connection object 
-      using (DbConnection cn = df.CreateConnection()) {
-        Console.WriteLine("Your connection object is a: {0}", cn.GetType().Name);
-        Console.WriteLine("With time out {0}", cn.ConnectionTimeout);
-        cn.ConnectionString = cnStr;
-        cn.Open();
+         // get factory provider 
+         DbProviderFactory df = DbProviderFactories.GetFactory(dp);
+         
+        
+         // now get the connection object 
+         using (DbConnection cn = df.CreateConnection()) {
+            Console.WriteLine("Your connection object is a: {0}", cn.GetType().Name);
+            Console.WriteLine("With time out {0}", cn.ConnectionTimeout);
+            cn.ConnectionString = cnStr;
+            cn.Open();
+            if (cn is SqlConnection) {
+               // Print out the version of the server 
+               // we need a cast to access such functionnlity 
+               Console.WriteLine("Version of your sql server is {0}",((SqlConnection)cn).ServerVersion);
+            }
+             
+            // Make a command object
+            DbCommand cmd = df.CreateCommand();
+            Console.WriteLine("Your command object is a: {0}", cmd.GetType().Name);
+            cmd.Connection = cn;
+            cmd.CommandText = "Select * from Custumer";
 
-        // Make a command object
-        DbCommand cmd = df.CreateCommand();
-        Console.WriteLine("Your command object is a: {0}", cmd.GetType().Name);
-        cmd.Connection = cn;
-        cmd.CommandText = "Select * from Custumer";
-
-        // Print out with data reader 
-        using (DbDataReader dr = cmd.ExecuteReader()) {
-          Console.WriteLine("\n***** Current Inventory *****");
-          while (dr.Read()) {
-            //Console.WriteLine("-> Car #{0} is a {1}",dr["CarID"], dr["Maker"] );
-            Console.WriteLine("-> Customer #{0} is: {1} {2}", dr[0], dr[1], dr[2]);
-          }
-        }
+            // Print out with data reader 
+            using (DbDataReader dr = cmd.ExecuteReader()) {
+               Console.WriteLine("\n***** Current Inventory *****");
+               while (dr.Read()) {
+                  //Console.WriteLine("-> Car #{0} is a {1}",dr["CarID"], dr["Maker"] );
+                  Console.WriteLine("-> Customer #{0} is: {1} {2}", dr[0], dr[1], dr[2]);
+               }
+            }
 
 
+         }
       }
-    }
-  }
+   }
 }
