00acae387360b363f180ed15071503f44782db47
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-06-30 11:55:21 +0200

    CSharp async await keyword

---
+++ FunWithCSharpAsync/FunWithCSharpAsync/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FunWithCSharpAsync {
+   public partial class Form1 : Form {
+      public Form1() {
+         InitializeComponent();
+      }
+
+      //private  void btnCallMethod_Click(object sender, EventArgs e) {
+      //   this.Text =  DoWork();
+      //}
+
+      //private string DoWork() {
+      //   Thread.Sleep(10000);
+      //   return "Done with Work";
+      //}
+
+
+      private async void btnCallMethod_Click(object sender, EventArgs e) {
+         this.Text = await DoWorkAsync();
+         //this.Text = getMessage();
+      }
+
+
+      // async and await keyword are added as decoration conventions 
+
+      private async Task<string> DoWorkAsync() {
+         return await Task.Run((Func<string>)getMessage);
+      }
+      
+
+      private string getMessage() {
+         Thread.Sleep(1000);
+         return "Done With Work";
+      }
+
+      // async method returning void 
+      private async Task methodreturningVoidAsync() {
+         await Task.Run(() => { Thread.Sleep(4000); });
+      }
+
+      private async void btnVoidMethodCall_Click(object sender, EventArgs e) {
+         await methodreturningVoidAsync();
+         MessageBox.Show("Done");
+      }
+
+      // async method with multiple awaits 
+
+      private async void btnMultiAwaits_Click(object sender, EventArgs e) {
+         await Task.Run(() => {Thread.Sleep(4000); });
+         MessageBox.Show("Done task 1");
+
+         await Task.Run(() => { Thread.Sleep(2000); });
+         MessageBox.Show("Done Task 2");
+
+         await Task.Run(() => { Thread.Sleep(2000); });
+         MessageBox.Show("Done Task 3");
+
+      }
+
+
+   }
+}
---
+++ PLINQDataProcessingWithCancellation/PLINQDataProcessingWithCancellation/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PLINQDataProcessingWithCancellation {
+   public partial class Form1 : Form {
+      private CancellationTokenSource cancelToken = new CancellationTokenSource();
+      
+      public Form1() {
+         InitializeComponent();
+      }
+
+      private void btnExecute_Click(object sender, EventArgs e) {
+         // Start a new task
+         cancelToken = new CancellationTokenSource();
+         Task.Factory.StartNew(() => {
+            ProcessIntData();
+         });
+      }
+
+      private void ProcessIntData() {
+         // Get a very large array of Ints 
+        int[] source = Enumerable.Range(1, 10000000).ToArray();
+
+         // Find the numbers where num%3 = 0, returned in descending order 
+         try {
+            int[] multipleOfThree = (from num in source.AsParallel().WithCancellation(cancelToken.Token)
+                                     where num % 3 == 0
+                                     orderby num descending
+                                     select num).ToArray();
+            MessageBox.Show(string.Format("Found {0} numbers that match query!", multipleOfThree.Count()));
+         }
+         catch (OperationCanceledException e) {
+            MessageBox.Show("Operation has been cancelled");
+         }
+      }
+
+      private void btnCancel_Click(object sender, EventArgs e) {
+         cancelToken.Cancel();
+      }
+   }
+}
