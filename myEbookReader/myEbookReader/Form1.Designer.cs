671362f1f8364a7825a79524bd629850862f6111
Mohamed <mohamed.sellahi@gmail.com>
2017-06-30 16:14:57 +0200

    EbookReader

---
+++ myEbookReader/myEbookReader/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace myEbookReader {
+   partial class Form1 {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing) {
+         if (disposing && (components != null)) {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent() {
+         this.btnDownload = new System.Windows.Forms.Button();
+         this.BtnGetStat = new System.Windows.Forms.Button();
+         this.TxtBook = new System.Windows.Forms.TextBox();
+         this.SuspendLayout();
+         // 
+         // btnDownload
+         // 
+         this.btnDownload.Location = new System.Drawing.Point(105, 301);
+         this.btnDownload.Name = "btnDownload";
+         this.btnDownload.Size = new System.Drawing.Size(75, 23);
+         this.btnDownload.TabIndex = 0;
+         this.btnDownload.Text = "Download";
+         this.btnDownload.UseVisualStyleBackColor = true;
+         this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+         // 
+         // BtnGetStat
+         // 
+         this.BtnGetStat.Location = new System.Drawing.Point(245, 301);
+         this.BtnGetStat.Name = "BtnGetStat";
+         this.BtnGetStat.Size = new System.Drawing.Size(75, 23);
+         this.BtnGetStat.TabIndex = 1;
+         this.BtnGetStat.Text = "GetStats";
+         this.BtnGetStat.UseVisualStyleBackColor = true;
+         this.BtnGetStat.Click += new System.EventHandler(this.BtnGetStat_Click);
+         // 
+         // TxtBook
+         // 
+         this.TxtBook.Location = new System.Drawing.Point(29, 25);
+         this.TxtBook.Multiline = true;
+         this.TxtBook.Name = "TxtBook";
+         this.TxtBook.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+         this.TxtBook.Size = new System.Drawing.Size(572, 270);
+         this.TxtBook.TabIndex = 2;
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(633, 337);
+         this.Controls.Add(this.TxtBook);
+         this.Controls.Add(this.BtnGetStat);
+         this.Controls.Add(this.btnDownload);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         this.Load += new System.EventHandler(this.Form1_Load);
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button btnDownload;
+      private System.Windows.Forms.Button BtnGetStat;
+      private System.Windows.Forms.TextBox TxtBook;
+   }
+}
+
---
+++ myEbookReader/myEbookReader/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myEbookReader {
+   public partial class Form1 : Form {
+      static public string theEbook = string.Empty;
+      public Form1() {
+         InitializeComponent();
+      }
+
+      private void Form1_Load(object sender, EventArgs e) {
+
+      }
+
+      private void btnDownload_Click(object sender, EventArgs e) {
+         WebClient wc = new WebClient();
+         wc.DownloadStringCompleted += (s, eArgs) => {
+            theEbook = eArgs.Result;
+            TxtBook.Text = theEbook;
+         };
+
+         // the project Gutenberg Ebook
+         wc.DownloadStringAsync(new Uri("http://www.gutenberg.org/files/55000/55000-0.txt"));
+      }
+
+      private void BtnGetStat_Click(object sender, EventArgs e) {
+         // get the words from the ebook ; 
+         string[] words = theEbook.Split(new char[] 
+         { ' ', '\u000A', ',', '.', ';', ':', '-', '?', '/' },
+         StringSplitOptions.RemoveEmptyEntries);
+
+         // Now find the ten most commen word 
+         string[] tenMostWord = null;
+         string longuestWord = string.Empty;
+
+
+         Parallel.Invoke(() => {
+            // Now find the ten most wommen words 
+            tenMostWord = FindMostCommonWord(words);
+         },
+         () => {
+
+            longuestWord = FindLongestWord(words);
+         }
+         );
+
+           
+
+         // Now that all tasks are complete, build a string show all
+         // stats message
+         StringBuilder bookStats = new StringBuilder("The ten most " +
+            "common words are: ");
+         foreach (string s in tenMostWord) {
+            bookStats.AppendLine(s);
+         }
+
+         bookStats.AppendFormat("Longuest word is: {0}", longuestWord);
+         bookStats.AppendLine();
+         MessageBox.Show(bookStats.ToString(), "Book Info");
+      }
+
+
+
+
+      private string FindLongestWord(string[] words) {
+         return (from w in words orderby w.Length descending select w).FirstOrDefault();
+      }
+
+      private string[] FindMostCommonWord(string[] words) {
+         var frequencyOrder = from word in words
+                              where word.Length > 6
+                              group word by word into g
+                              orderby g.Count() descending
+                              select g.Key;
+         return (frequencyOrder.Take(10)).ToArray();
+      }
+   }
+}
