cfc00d5fda4835377679c09a88cf1d2b2d8cbe2d
Mohamed <mohamed.sellahi@gmail.com>
2017-07-29 22:36:40 +0200

    Using Strongly typed autogenerated data sets

--- AutoLotDAL version three/AutoLotDAL/AutoLotDataSet.Designer.cs
+++ AutoLotDAL version three/AutoLotDAL/AutoLotDataSet.Designer.cs
@@ -3501,6 +3501,85 @@ SELECT CustID, FirstName, LastName FROM Custumer WHERE (CustID = @CustID)";
     }
     
     /// <summary>
+    ///Represents the connection and commands used to retrieve and save data.
+    ///</summary>
+    [global::System.ComponentModel.DesignerCategoryAttribute("code")]
+    [global::System.ComponentModel.ToolboxItem(true)]
+    [global::System.ComponentModel.DataObjectAttribute(true)]
+    [global::System.ComponentModel.DesignerAttribute("Microsoft.VSDesigner.DataSource.Design.TableAdapterDesigner, Microsoft.VSDesigner" +
+        ", Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
+    [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+    public partial class QueriesTableAdapter : global::System.ComponentModel.Component {
+        
+        private global::System.Data.IDbCommand[] _commandCollection;
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        protected global::System.Data.IDbCommand[] CommandCollection {
+            get {
+                if ((this._commandCollection == null)) {
+                    this.InitCommandCollection();
+                }
+                return this._commandCollection;
+            }
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        private void InitCommandCollection() {
+            this._commandCollection = new global::System.Data.IDbCommand[1];
+            this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
+            ((global::System.Data.SqlClient.SqlCommand)(this._commandCollection[0])).Connection = new global::System.Data.SqlClient.SqlConnection(global::AutoLotDAL.Properties.Settings.Default.AutoLotConnectionString);
+            ((global::System.Data.SqlClient.SqlCommand)(this._commandCollection[0])).CommandText = "dbo.GetPetName";
+            ((global::System.Data.SqlClient.SqlCommand)(this._commandCollection[0])).CommandType = global::System.Data.CommandType.StoredProcedure;
+            ((global::System.Data.SqlClient.SqlCommand)(this._commandCollection[0])).Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@RETURN_VALUE", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.ReturnValue, 10, 0, null, global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            ((global::System.Data.SqlClient.SqlCommand)(this._commandCollection[0])).Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@carID", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 10, 0, null, global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            ((global::System.Data.SqlClient.SqlCommand)(this._commandCollection[0])).Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@petName", global::System.Data.SqlDbType.Char, 10, global::System.Data.ParameterDirection.InputOutput, 0, 0, null, global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        public virtual int GetPetName(global::System.Nullable<int> carID, ref string petName) {
+            global::System.Data.SqlClient.SqlCommand command = ((global::System.Data.SqlClient.SqlCommand)(this.CommandCollection[0]));
+            if ((carID.HasValue == true)) {
+                command.Parameters[1].Value = ((int)(carID.Value));
+            }
+            else {
+                command.Parameters[1].Value = global::System.DBNull.Value;
+            }
+            if ((petName == null)) {
+                command.Parameters[2].Value = global::System.DBNull.Value;
+            }
+            else {
+                command.Parameters[2].Value = ((string)(petName));
+            }
+            global::System.Data.ConnectionState previousConnectionState = command.Connection.State;
+            if (((command.Connection.State & global::System.Data.ConnectionState.Open) 
+                        != global::System.Data.ConnectionState.Open)) {
+                command.Connection.Open();
+            }
+            int returnValue;
+            try {
+                returnValue = command.ExecuteNonQuery();
+            }
+            finally {
+                if ((previousConnectionState == global::System.Data.ConnectionState.Closed)) {
+                    command.Connection.Close();
+                }
+            }
+            if (((command.Parameters[2].Value == null) 
+                        || (command.Parameters[2].Value.GetType() == typeof(global::System.DBNull)))) {
+                petName = null;
+            }
+            else {
+                petName = ((string)(command.Parameters[2].Value));
+            }
+            return returnValue;
+        }
+    }
+    
+    /// <summary>
     ///TableAdapterManager is used to coordinate TableAdapters in the dataset to enable Hierarchical Update scenarios
     ///</summary>
     [global::System.ComponentModel.DesignerCategoryAttribute("code")]
---
+++ StronglyTypedDataSetConsoleClient/StronglyTypedDataSetConsoleClient/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoLotDAL.AutoLotDataSetTableAdapters;
+using AutoLotDAL;
+
+namespace StronglyTypedDataSetConsoleClient {
+   class Program {
+      static void Main(string[] args) {
+         Console.WriteLine("Fun with Strongly Typed Datasets *****");
+
+         AutoLotDataSet.InventoryDataTable table =
+            new AutoLotDataSet.InventoryDataTable();
+
+         InventoryTableAdapter dAdapt = new InventoryTableAdapter();
+
+         dAdapt.Fill(table);
+         PrintInventory(table);
+         AddRecords(table,dAdapt);
+         Console.WriteLine("Somme rows have been added");
+         PrintInventory(table);
+         Console.WriteLine("After removing");
+         RemoveRecord(table, dAdapt);
+         PrintInventory(table);
+         CallStoredProc();
+         PrintInventory(table);
+      }
+
+
+      static void PrintInventory(AutoLotDataSet.InventoryDataTable dt) {
+         // PRint out the column names 
+         for (int curCol = 0; curCol < dt.Columns.Count; curCol++) {
+            Console.Write(dt.Columns[curCol].ColumnName + "\t");
+         }
+         Console.WriteLine();
+
+         // Print the data 
+         for (int curRow = 0; curRow < dt.Rows.Count; curRow++) {
+            for (int curCol = 0; curCol < dt.Columns.Count; curCol++) {
+               Console.Write(dt.Rows[curRow][curCol].ToString().Trim() + "\t");
+            }
+            Console.WriteLine();
+         }
+
+      }
+
+      public static void AddRecords(AutoLotDataSet.InventoryDataTable dt,
+         InventoryTableAdapter dAdapt) {
+         try {
+            //Get strongly datarow
+            AutoLotDataSet.InventoryRow newRow = dt.NewInventoryRow();
+            // Fill
+            newRow.CarID = 999;
+            newRow.Color = "Purple";
+            newRow.Maker = "BMW";
+            newRow.PetName = "Saku";
+
+            // Insert the new row 
+            dt.AddInventoryRow(newRow);
+
+            // add one more row, using the overloaded add method 
+            dt.AddInventoryRow(8888, "Yugo", "Green", "Zippy");
+
+            // update database
+            dAdapt.Update(dt);
+
+         }
+         catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+         }
+      }
+
+      public static void RemoveRecord(AutoLotDataSet.InventoryDataTable dt,
+         InventoryTableAdapter dAdapt) {
+         try {
+            AutoLotDataSet.InventoryRow rowToDelete = dt.FindByCarID(999);
+            dAdapt.Delete(rowToDelete.CarID, rowToDelete.Maker,
+               rowToDelete.Color, rowToDelete.PetName);
+            rowToDelete = dt.FindByCarID(888);
+            dAdapt.Delete(rowToDelete.CarID, rowToDelete.Maker,
+               rowToDelete.Color, rowToDelete.PetName);
+
+         }
+         catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+         }
+      }
+
+      // Invocking stored procedures
+
+      public static void CallStoredProc() {
+         try {
+            QueriesTableAdapter q = new QueriesTableAdapter();
+            Console.WriteLine("Enter ID of the car to look up: ");
+            string ID = Console.ReadLine();
+            string carName = string.Empty;
+            q.GetPetName(int.Parse(ID), ref carName);
+            Console.WriteLine("CarID {0} has the name of {1}",ID, carName);
+         }
+         catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+         }
+      }
+   }
+}
